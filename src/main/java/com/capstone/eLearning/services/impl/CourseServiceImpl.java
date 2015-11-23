4180e1f0bdaa9d8ecca2644f9ecc910e365f7ade
KrishnaReddy Kalyanam <krishnareddy2k@gmail.com>
2015-11-23 00:39:06 -0800

    404 error on course services

--- src/main/java/com/capstone/eLearning/services/impl/CourseServiceImpl.java
+++ src/main/java/com/capstone/eLearning/services/impl/CourseServiceImpl.java
@@ -1,5 +1,7 @@
 package com.capstone.eLearning.services.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
