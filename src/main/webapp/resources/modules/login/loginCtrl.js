22f720bcf43e2285405e7a1ed6ce9d73a07c3d13
KrishnaReddy Kalyanam <krishnareddy2k@gmail.com>
2015-11-22 23:59:20 -0800

    ui_changes

--- src/main/webapp/resources/modules/login/loginCtrl.js
+++ src/main/webapp/resources/modules/login/loginCtrl.js
@@ -21,9 +21,12 @@ function LoginCtrl(UserService, $location, AuthenticationService, FlashService,
 		vm.dataLoading = true;
 		UserService.Login(vm.user.username, vm.user.password).then(function(response) {
 			if (response.success) {
+				vm.user=response.data;
 				FlashService.Success('Registration successful', true);
 				AuthenticationService.SetCredentials(vm.user.username, vm.user.password);
+				AuthenticationService.SetUser(vm.user);
 				$state.go('dashboard');
+				
 			} else {
 				FlashService.Error("Invalid username or password");
 				vm.error = "Invalid username or password";
