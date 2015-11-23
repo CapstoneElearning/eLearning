a92fd7cc91aff7e9118a3baec484fae1930895f3
KrishnaReddy Kalyanam <krishnareddy2k@gmail.com>
2015-11-22 23:45:29 -0800

    404 error on course services

--- src/main/java/com/capstone/eLearning/services/CourseService.java
+++ src/main/java/com/capstone/eLearning/services/CourseService.java
@@ -8,4 +8,5 @@ public interface CourseService {
 	Course retrieve(Long courseId) throws ServiceException;
 	void delete(Long courseId) throws ServiceException;
 	void update(Long courseId, String startd_date, String end_date, double credits, int instructor, int active) throws ServiceException;
+	public List<Course> getAll();
 }
