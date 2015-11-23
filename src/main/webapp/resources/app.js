dc00f884e51dea08b9ee289f6edb52abb5cc738f
KrishnaReddy Kalyanam <krishnareddy2k@gmail.com>
2015-11-22 23:53:42 -0800

    ui_changes

--- src/main/webapp/resources/app.js
+++ src/main/webapp/resources/app.js
@@ -1,163 +1,194 @@
-(function(){
-'use strict';
-/**
- * Created by KrishnaReddy on 10/6/2015.
- */
-angular
-    .module('myApp', ['ngRoute', 'ngCookies','ngMessages','ui.router'])
-    .config(config)
-    .run(run);
+(function() {
+	'use strict';
+	/**
+	 * Created by KrishnaReddy on 10/6/2015.
+	 */
+	angular.module('myApp',	[ 'ngRoute', 'ngCookies', 'ngMessages', 'ui.router' ]).config(config).run(run);
 
-config.$inject = ['$routeProvider', '$locationProvider','$httpProvider','$urlRouterProvider','$stateProvider'];
-function config($routeProvider,$locationProvider,$httpProvider,$urlRouterProvider,$stateProvider){
-    $stateProvider
-        .state('home',{
-        	url:'/home',
-            views: {
-            	'courses': {
-                    templateUrl: '../resources/modules/course/courses.html' 
-                }
-            }
-        
-        })
-        .state('login',{
-        	url:'/login',
-            views: {    
-            	'content': {
-                    templateUrl: '../resources/modules/login/login.html' ,
-                }
-            }
-        
-        })
-        
-        .state('signup',{
-        	url:'/signup',
-            views: {              	
-                'content': {
-                    templateUrl: '../resources/modules/signup/signup.html' ,
-                }
-            }
-        
-        })
-         .state('forgotPwd',{
-        	 url:'/forgotPwd',
-            views: {              
-                'content': {
-                    templateUrl: '../resources/modules/signup/forgot.html' ,
-                }
-            }
-        
-        })
-         .state('success',{
-        	 url:'/success',
-            views: {              	
-                'content': {
-                    templateUrl: '../resources/modules/signup/success.html' ,
-                }
-            }
-        
-        })
-        
-        .state('about',{
-            views: {              
-                'content': {
-                    templateUrl: '../resources/modules/about/aboutus.html' ,
-                }
-            }
-        
-        })
-        
-        .state('contact',{
-            views: {              
-                'content': {
-                    templateUrl: '../resources/modules/contact/contactus.html' ,
-                }
-            }
-        
-        })
-        
-          // Courses
-      .state('dashboard', {
-        url: '/dashboard',
-        views: {              
-            'content': {
-            	templateUrl: '../resources/modules/dashboard/dashboard.html',
-            },
-            resolve:{
-                logincheck:checkLoggedin
-            }
-        }
-      
-      })
-       
-        // Courses
-      .state('courses', {
-        url: '/courses',
-        templateUrl: '../resources/modules/course/courses-index.html',
-        controller: 'coursesIndexCtrl'
-        // resolve: ?
-      })
-      .state('newCourse', {
-        url: '/courses/new',
-        templateUrl: '../resources/modules/course/Course-new.html',
-        controller: 'NewCourseCtrl'
-      })
-      .state('showCourse', {
-        url: '/courses/:id',
-        templateUrl: '../resources/modules/course/Course-show.html',
-        controller: 'ShowCourseCtrl'
-      })
-      .state('editCourse', {
-        url: '/courses/:id/edit',
-        templateUrl: '../resources/modules/course/Course-edit.html',
-        controller: 'EditCourseCtrl'
-      });
-    
-	$urlRouterProvider.otherwise('/home');
+	config.$inject = [ '$routeProvider', '$locationProvider', '$httpProvider','$urlRouterProvider', '$stateProvider' ];
+	function config($routeProvider, $locationProvider, $httpProvider,$urlRouterProvider, $stateProvider) {
+		$stateProvider
+				.state('home',{
+							url : '/home',
+							views : {
+								'content' : {
+									templateUrl : '/eLearning/resources/modules/course/courses.html',
+									controller : 'CourseCtrl'
+								}
+							}
 
-    $httpProvider.defaults.useXDomain = true;
-    delete $httpProvider.defaults.headers.common['X-Requested-With'];
-}
+						})
+				.state(
+						'login',
+						{
+							url : '/login',
+							views : {
+								'content' : {
+									templateUrl : '/eLearning/resources/modules/login/login.html',
+								}
+							}
 
-run.$inject = ['$rootScope', '$location', '$cookieStore', '$http'];
+						})
 
-var checkLoggedin=function checkLogin($q,$timeout,$http,$location,$rootScope,UserService){
+				.state(
+						'signup',
+						{
+							url : '/signup',
+							views : {
+								'content' : {
+									templateUrl : '/eLearning/resources/modules/signup/signup.html',
+								}
+							}
 
-    var deferred=$q.defer();
+						})
+				.state(
+						'forgotPwd',
+						{
+							url : '/forgotPwd',
+							views : {
+								'content' : {
+									templateUrl : '/eLearning/resources/modules/forgot/forgot.html',
+								}
+							}
 
-    UserService.Login(vm.username, vm.password).then(function(user) {
-    	$rootScope.errorMessage=null;
-        if(user.role_id !== 0)
-        {
-            $rootScope.currentUser = user;
-            $rootScope.currentUser.isAdmin = true;
-            deferred.resolve();
-        }else{
-            $rootScope.errorMessage='You need to log in.';
-            deferred.reject();
-            $location.url('/login')
-        }
-	});    
+						})
+				.state(
+						'success',
+						{
+							url : '/success',
+							views : {
+								'content' : {
+									templateUrl : '/eLearning/resources/modules/signup/success.html',
+								}
+							}
 
-    return deferred.promise;
-};
+						})
 
-function run($rootScope, $location, $cookieStore, $http) {
-    // keep user logged in after page refresh
-    $rootScope.globals = $cookieStore.get('globals') || {};
-    if ($rootScope.globals.currentUser) {
-        $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
-    }
+				.state(
+						'about',
+						{
+							views : {
+								'content' : {
+									templateUrl : '/eLearning/resources/modules/about/aboutus.html',
+								}
+							}
 
-    $rootScope.$on('$locationChangeStart', function (event, next, current) {
-        // redirect to login page if not logged in and trying to access a restricted page
-        var restrictedPage = $.inArray($location.path(), ['/home','/login', '/signup','/about','/contact','/success']) === -1;
-        var loggedIn = $rootScope.globals.currentUser;
-        if($location.path()==""){
-        	$location.path('/home');
-        }else if (restrictedPage && !loggedIn) {
-            $location.path('/login');
-        }
-    });
-}
+						})
+
+				.state(
+						'contact',
+						{
+							views : {
+								'content' : {
+									templateUrl : '/eLearning/resources/modules/contact/contactus.html',
+								}
+							}
+
+						})
+
+				// Courses
+				.state(
+						'dashboard',
+						{
+							url : '/dashboard',
+							views : {
+								'content' : {
+									templateUrl : '/eLearning/resources/modules/dashboard/dashboard.html',
+								},
+								resolve : {
+									logincheck : checkLoggedin
+								}
+							}
+
+						})
+
+				// Courses
+				.state('courses',{
+							abstract : true,
+							url : '/courses',
+							templateUrl : '/eLearning/resources/modules/course/courses.html',
+							controller : 'CourseCtrl'
+
+						})
+						
+				.state('courses.enroll',{
+		            	url : '/enroll/:id',
+						views: {
+				            'content@': {
+								templateUrl : '/eLearning/resources/modules/course/course-show.html',
+								controller : 'EnrollCourseCtrl'
+				            }
+						}
+						})
+				.state(
+						'newCourse',
+						{
+							url : '/new',
+							templateUrl : '/eLearning/resources/modules/course/course-new.html',
+							controller : 'NewCourseCtrl'
+						})
+				.state(
+						'editCourse',
+						{
+							url : '/:id/edit',
+							templateUrl : '/eLearning/resources/modules/course/course-edit.html',
+							controller : 'EditCourseCtrl'
+						});
+
+		$urlRouterProvider.otherwise('/home');
+
+		$httpProvider.defaults.useXDomain = true;
+		delete $httpProvider.defaults.headers.common['X-Requested-With'];
+		//    
+		// $locationProvider.html5Mode({
+		// enabled: true,
+		// requireBase: false
+		// });
+
+	}
+
+	run.$inject = [ '$rootScope', '$location', '$cookieStore', '$http' ];
+
+	var checkLoggedin = function checkLogin($q, $timeout, $http, $location,
+			$rootScope, UserService) {
+
+		var deferred = $q.defer();
+
+		UserService.Login(vm.username, vm.password).then(function(user) {
+			$rootScope.errorMessage = null;
+			if (user.role_id !== 0) {
+				$rootScope.currentUser = user;
+				$rootScope.currentUser.isAdmin = true;
+				deferred.resolve();
+			} else {
+				$rootScope.errorMessage = 'You need to log in.';
+				deferred.reject();
+				$location.url('/login')
+			}
+		});
+
+		return deferred.promise;
+	};
+
+	function run($rootScope, $location, $cookieStore, $http) {
+		// keep user logged in after page refresh
+		$rootScope.globals = $cookieStore.get('globals') || {};
+		if ($rootScope.globals.currentUser) {
+			$http.defaults.headers.common['Authorization'] = 'Basic '
+					+ $rootScope.globals.currentUser.authdata; // jshint
+																// ignore:line
+		}
+
+		
+		  $rootScope.$on('$locationChangeStart', function (event, next,
+		  current) { // redirect to login page if not logged in and trying to access a restricted page 
+		  var restrictedPage = $.inArray($location.path(), ['/home','/login', '/signup','/about','/contact','/success','/courses']) === -1; 
+		  var  loggedIn = $rootScope.globals.currentUser;
+		  if($location.path()==""){
+			  $location.path('/home');
+		  }else if (restrictedPage && !loggedIn) {
+			  $location.path('/login'); }
+		  });
+		 
+	}
 })();
